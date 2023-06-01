a454e3e36890a034e149358a5b7b7c7f5d31e2e2
Hillay Amir <71714540+Destorntw@users.noreply.github.com>
2023-06-01 14:47:35 +0300

    Create registry-5000.sh

---
+++ registry-5000.sh
@@ -0,0 +1,11 @@
+echo 'podman run --name registry -d -p 5000:5000 \
+-v /var/registry/data:/var/lib/registry:z \
+-v /var/registry/auth:/auth:z \
+-e "REGISTRY_AUTH=htpasswd" \
+-e "REGISTRY_AUTH_HTPASSWD_REALM=Registry" \
+-e "REGISTRY_HTTP_SECRET=ALongRandomSecretForRegistry" \
+-e REGISTRY_AUTH_HTPASSWD_PATH=/auth/htpasswd \
+-v /var/registry/certs:/certs:z \
+-e REGISTRY_HTTP_TLS_CERTIFICATE=/certs/domain.crt \
+-e REGISTRY_HTTP_TLS_KEY=/certs/domain.key \
+docker.io/library/registry:2' > /var/registry/tools/start_registry.sh
