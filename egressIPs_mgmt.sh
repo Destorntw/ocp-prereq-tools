ab9ba534ee1600dd9ba4a1504d5f003580e7576e
Hillay Amir <71714540+Destorntw@users.noreply.github.com>
2023-06-08 17:15:31 +0300

    Update egressIPs_mgmt.sh
    fix after internal run

--- egressIPs_mgmt.sh
+++ egressIPs_mgmt.sh
@@ -1,146 +1,177 @@
 !/bin/bash
-echo "Welcome to the hostsubnet & netnamespace (Openshift SDN objects) patcher script"
+echo "Welcome to the hostsubnet $ netnamespace (Openshift SDN objects) patcher script"
+
 echo "Would you like to: \
-(1) - Add egress ip's to worker nodes \
-(2) - Add egress ip's to a project (ns)"
+(1) - Add egress ip's to a project \
+(2) - Add egress ip's to worker nodes"
 read usr_choice
- if [ $usr_choice -eq 1 ]
- then
 
-  echo "What is your node name prefix \
-  exmaple: app-xx.ocp.example.com \
-  the name prefix is "app-" \
-  dont forget the "-" after node name prefix"
-  read=prefix
+if [ $usr_choice -eq 1 ]
+then
 
-  echo "What is your domain prefix and suffix \
-  exmaple: app-xx.ocp.example.com \
-  the full domain is ".ocp.example.com" \
-  dont forget the dot before cluster name prefix ".ocp""
-  read=domain
+    echo "What is your node name prefix exmaple:app-xx.ocp.example.com the name prefix is "app-" dont forget the "-" after node name prefix"
+    read=prefix
 
-  echo -n "How many worker nodes do you have in your cluster \
-  please note the script patches all worker nodes \
-  exmaple: 67"
-  read machine_count
+    echo "What is your domain prefix and suffix exmaple:app-xx.ocp.example.com the full domain is ".ocp.example.com" dont forget the dot before cluster name prefix ".ocp""
+    read=domain
 
-  echo Your have enterd you have $machine_count worker nodes
+    echo "How many worker nodes do you have in your cluster please note the script patches all worker nodes exmaple: 67"
+    read machine_count
 
-  echo -n "How many IP addresses would you like to patch *SELECT NUMBER* - (1 IP) (2 IP's) (3 IP's) \
-  Please note if you patch the same hostsubnet object twice than the patch OVER-RIDES the previous one!"
-  read ipcount
+    echo Your have enterd you have $machine_count worker nodes
 
-  if [ $ipcount -eq 1 ]
-  then
+    echo "How many IP addresses would you like to patch *SELECT NUMBER* - (1 IP) (2 IP's) (3 IP's) \
+    Please note if you patch the same hostsubnet object twice than the patch OVER-RIDES the previous one!"
+    read ipcount
 
-    echo "Enter your IP address"
-    read ip1
+    if [ $ipcount -eq 1 ]
+    then
 
-    for ((i=1; i<=$machine_count; i++ ));
-    do
-      name="$prefix$i$domain"
-      echo adding hostsubnet to node: $name
-      oc patch hostsubnet $name --type=merge -p '{"egressIPs": ["'"$ip1"'"]}'
-    done
+            echo "Enter your IP address"
+            read ip1
 
-  elif [ $ipcount -eq 2 ]
-  then
+        for ((i=1; i<=$machine_count; i++ ));
+        do
+                name="$prefix$i$domain"
+                echo adding hostsubnet to node: $name
+                oc patch hostsubnet $name --type=merge -p '{"egressIPs": ["'"$ip1"'"]}'
+        done
 
-    echo "Enter your IP address"
-    read ip1
-    echo "Enter your second IP address"
-    read ip2
+        sleep 1s
 
-    for ((i=1; i<=$machine_count; i++ ));
-    do
-      name="$prefix$i$domain"
-      echo adding hostsubnet to node: $name
-      oc patch hostsubnet $name --type=merge -p '{"egressIPs": ["'"$ip1"'", "'"$ip2"'"]}'
-    done
+        echo "A report will be generated indicating all of the updated hostsubnet's for each node \
+        file path is ./hostsub_report.txt"
 
-  elif [ $ipcount -eq 3 ]
-  then
+        oc get hostsubnet | awk '{print $1, $5, $6, $7}' >> ./hostsub_report.txt
+        echo "Report genration finished"
 
-    echo "Enter your IP address"
-    read ip1
-    echo "Enter your second IP address"
-    read ip2
-    echo "Enter your third IP address"
-    read ip3
+    elif [ $ipcount -eq 2 ]
+    then
 
-    for ((i=1; i<=$machine_count; i++ ));
-    do
-      name="$prefix$i$domain"
-      echo adding hostsubnet to node: $name
-      oc patch hostsubnet $name --type=merge -p '{"egressIPs": ["'"$ip1"'", "'"$ip2"'", "'"$ip3"'"]}'
-    done
+            echo "Enter your IP address"
+            read ip1
+            echo "Enter your second IP address"
+            read ip2
 
-  else
-    echo "Wrong choice please re-run the script"
-  fi
+            for ((i=1; i<=$machine_count; i++ ));
+            do
+                    name="$prefix$i$domain"
+                    echo adding hostsubnet to node: $name
+                    oc patch hostsubnet $name --type=merge -p '{"egressIPs": ["'"$ip1"'", "'"$ip2"'"]}'
+            done
+
+            sleep 1s
+
+            echo "A report will be generated indicating all of the updated hostsubnet's for each node \
+            file path is ./hostsub_report.txt"
+
+            oc get hostsubnet | awk '{print $1, $5, $6, $7}' >> ./hostsub_report.txt
+            echo "Report genration finished"
+
+    elif [ $ipcount -eq 3 ]
+    then
+
+            echo "Enter your IP address"
+            read ip1
+            echo "Enter your second IP address"
+            read ip2
+            echo "Enter your third IP address"
+            read ip3
+
+            for ((i=1; i<=$machine_count; i++ ));
+            do
+                    name="$prefix$i$domain"
+                    echo adding hostsubnet to node: $name
+                    oc patch hostsubnet $name --type=merge -p '{"egressIPs": ["'"$ip1"'", "'"$ip2"'", "'"$ip3"'"]}'
+            done
+
+            sleep 1s
+
+            echo "A report will be generated indicating all of the updated hostsubnet's for each node \
+            file path is ./hostsub_report.txt"
+
+            oc get hostsubnet | awk '{print $1, $5, $6, $7}' >> ./hostsub_report.txt
+            echo "Report genration finished"
+
+    else
+            echo "Wrong choice please re-run the script"
+    fi
 
 
 # Pathcer part 2 - patch netnamespace (adding egressIP)
- elif [ $usr_choice -eq 2 ]
- then
+elif [ $usr_choice -eq 2 ]
+then
 
-  echo -n "How many IP addresses would you like to add to a project  *SELECT NUMBER* - (1 IP) (2 IP's) (3 IP's) \
-  Please note if you patch the same netnamespace object twice than the patch OVER-RIDES the previous one!"
-  read ipcount_proj
+    echo "How many IP addresses would you like to add to a project  *SELECT NUMBER* - (1 IP) (2 IP's) (3 IP's) \
+    Please note if you patch the same netnamespace object twice than the patch OVER-RIDES the previous one!"
+    read ipcount_proj
 
-  if [ $ipcount_proj -eq 1 ]
-  then
-    echo "Enter your project name"
-    read proj_name
-    echo "Enter your IP address"
-    read ip1
+    if [ $ipcount_proj -eq 1 ]
+    then
+            echo "Enter your project name"
+            read proj_name
+            echo "Enter your IP address"
+            read ip1
 
-      echo adding egress ip to project: $proj_name
-      oc patch netnamespace $proj_name --type=merge -p '{"egressIPs": ["'"$ip1"'"]}'
+            echo adding egress ip to project: $proj_name
+            oc patch netnamespace $proj_name --type=merge -p '{"egressIPs": ["'"$ip1"'"]}'
 
-  elif [ $ipcount_proj -eq 2 ]
-  then
-    echo "Enter your project name"
-    read proj_name
-    echo "Enter your first IP address"
-    read ip1
-    echo "Enter your second IP address"
-    read ip2
+            sleep 1s
 
-    echo adding egress ip to project: $proj_name
-    oc patch netnamespace $proj_name --type=merge -p '{"egressIPs": ["'"$ip1"'", "'"$ip2"'"]}'
+            echo "A report will be generated indicating all of the updated egressIP's for each project\
+            file path is ./prject_egressIPs_report.txt"
+
+            oc get netnamespace | awk '{print $1, $3, $4, $5}' >> ./prject_egressIPs_report.txt
+            echo "Report genration finished"
 
 
-  elif [ $ipcount_proj -eq 2 ]
-  then
+    elif [ $ipcount_proj -eq 2 ]
+    then
+            echo "Enter your project name"
+            read proj_name
+            echo "Enter your first IP address"
+            read ip1
+            echo "Enter your second IP address"
+            read ip2
 
-    echo "Enter your project name"
-    read proj_name
-    echo "Enter your first IP address"
-    read ip1
-    echo "Enter your second IP address"
-    read ip2
-    echo "Enter your third IP address"
-    read ip3
+            echo adding egress ip to project: $proj_name
+            oc patch netnamespace $proj_name --type=merge -p '{"egressIPs": ["'"$ip1"'", "'"$ip2"'"]}'
 
-    echo adding egress ip to project: $proj_name
-    oc patch netnamespace $proj_name --type=merge -p '{"egressIPs": ["'"$ip1"'", "'"$ip2"'","'"$ip3"'"]}'
+            sleep 1s
 
-  else
-          echo "Wrong choice please re-run the script"
-  fi
+            echo "A report will be generated indicating all of the updated egressIP's for each project\
+            file path is ./prject_egressIPs_report.txt"
 
- else
-          echo "Wrong choice please re-run the script"
- fi
+            oc get netnamespace | awk '{print $1, $3, $4, $5}' >> ./prject_egressIPs_report.txt
+            echo "Report genration finished"
 
-sleep 1s
-echo -n "A report will be generated indicating all of the updated hostsubnet's for each node\
-file path is ./hostsub_report.txt"
-oc get hostsubnet | awk '{print $1, $5, $6, $7}' >> ./hostsub_report.txt
-echo "Report generation finished"
+    elif [ $ipcount_proj -eq 2 ]
+    then
 
-echo -n "A report will be generated indicating all of the updated egressIP's for each project\
-file path is ./prject_egressIPs_report.txt"
-oc get hostsubnet | awk '{print $1, $5, $6, $7}' >> ./prject_egressIPs_report.txt
-echo "Report generation finished"
+            echo "Enter your project name"
+            read proj_name
+            echo "Enter your first IP address"
+            read ip1
+            echo "Enter your second IP address"
+            read ip2
+            echo "Enter your third IP address"
+            read ip3
+
+            echo adding egress ip to project: $proj_name
+            oc patch netnamespace $proj_name --type=merge -p '{"egressIPs": ["'"$ip1"'", "'"$ip2"'","'"$ip3"'"]}'
+
+            sleep 1s
+
+            echo -n "A report will be generated indicating all of the updated egressIP's for each project\
+            file path is ./prject_egressIPs_report.txt"
+
+            oc get netnamespace | awk '{print $1, $3, $4, $5}' >> ./prject_egressIPs_report.txt
+            echo "Report genration finished"
+
+    else
+        echo "Wrong choice please re-run the script"
+    fi
+
+else
+echo "Wrong choice please re-run the script"
+fi
